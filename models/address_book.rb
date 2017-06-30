adc3646de7dacd4ab48e47b40b66040fbf66a374
Andrew Neidley <neidz44@gmail.com>
2017-06-30 00:55:36 -0500

    Assignment 4 - Address Bloc-Models: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -6,19 +6,25 @@ require_relative 'entry'
         def initialize
             @entries = []
         end
-        
 
         def add_entry(name, phone_number, email)
 
-        index = 0
-        entries.each do |entry|
+            index = 0
+            entries.each do |entry|
 
-        if name < entry.name
-            break
+                if name < entry.name
+                    break
+                end
+                index+= 1
         end
-        index+= 1
+            entries.insert(index, Entry.new(name, phone_number, email))
         end
 
-        entries.insert(index, Entry.new(name, phone_number, email))
+        def remove_entry(name, phone_number, email)
+            entries.each_with_index do |number, index|
+                if number.to_s = "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+                    entries.delete_at(index)
+                end
+        end
     end
 end
